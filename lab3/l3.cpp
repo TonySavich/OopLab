3a84e4550a78c9a0ee0ff55e31db0bd705db645b
Anton <ASSavich@stud.kantiana.ru>
2023-11-30 18:07:37 +0200

    .

---
+++ lab3/l3.cpp
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+
+const int N = 1000;
+
+
+template <typename T>
+class Array3d {
+public:
+
+    Array3d(int i, int j, int k) {
+        m_t = i * j * k;
+        m_i = i;
+        m_j = j;
+        m_k = k;
+
+    }
+    void SetAll() {
+        for (int i = 0; i < m_t; i++) {
+            std::cin >> mas[i];
+        }
+    }
+
+
+
+    void Fill(int tmp) {
+        for (int i = 0; i < m_t; i++) {
+            mas[i] = tmp;
+        }
+    }
+
+    int Element(int i, int j, int k) {
+        return mas[i + j * m_i + k * m_i * m_j];
+}
+    
+
+    void GetValues0(int i) {
+
+        for (int j = 0; j < m_j; j++) {
+            for (int k = 0; k < m_k; k++) {
+                std::cout << mas[i + j * m_i + k * m_i * m_j] << " ";
+    
+            }
+            std::cout << std::endl;;
+        }
+    }
+
+    void GetValues1(int j) {
+
+        for (int i = 0; i < m_i; i++) {
+            for (int k = 0; k < m_k; k++) {
+                std::cout << mas[i + j * m_i + k * m_i * m_j] << " ";
+   
+            }
+            std::cout << std::endl;;
+        }
+    }
+
+
+    void GetValues2(int k) {
+  
+        for (int i = 0; i < m_i; i++) {
+            for (int j = 0; j < m_j; j++) {
+                std::cout << mas[i + j * m_i + k * m_i * m_j] << " ";
+               
+            }
+            std::cout << std::endl;;
+        }
+    }
+
+
+    void GetValues01(int i, int j) {
+ 
+            for (int k = 0; k < m_k; k++) {
+                std::cout << mas[i + j*m_i + k*m_i*m_j] << " ";
+         
+            }
+     
+    }
+
+    void GetValues02(int i, int k) {
+
+        for (int j = 0; j < m_j; j++) {
+            std::cout << mas[i + j * m_i + k * m_i * m_j] << " ";
+        
+        }
+
+    }
+
+    void GetValues12(int j, int k) {
+      
+        for (int i = 0; i < m_i; i++) {
+            std::cout << mas[i + j * m_i + k * m_i * m_j] << " ";
+
+        }
+
+    }
+
+
+
+
+
+    void SetValues0(int i) {
+        for (int j = 0; j < m_j; j++) {
+            for (int k = 0; k < m_k; k++) {
+                std::cin>> mas[i + j * m_i + k * m_i * m_j];
+            }
+        }
+    }
+
+    void SetValues1(int j) {
+        for (int i = 0; i < m_i; i++) {
+            for (int k = 0; k < m_k; k++) {
+                std::cin >> mas[i + j * m_i + k * m_i * m_j];
+            }
+        }
+    }
+
+
+    void SetValues2(int k) {
+        for (int i = 0; i < m_i; i++) {
+            for (int j = 0; j < m_j; j++) {
+                std::cin >> mas[i + j * m_i + k * m_i * m_j];
+            }
+        }
+    }
+
+
+    void SetValues01(int i, int j) {
+
+        for (int k = 0; k < m_k; k++) {
+            std::cin >> mas[i + j * m_i + k * m_i * m_j];
+
+        }
+
+    }
+
+
+    void SetValues02(int i, int k) {
+
+        for (int j = 0; j < m_j; j++) {
+            std::cin >> mas[i + j * m_i + k * m_i * m_j];
+
+        }
+
+    }
+
+    void SetValues12(int j, int k) {
+
+        for (int i = 0; i < m_i; i++) {
+            std::cin >> mas[i + j * m_i + k * m_i * m_j];
+
+        }
+
+    }
+
+private:
+    int m_i, m_j, m_k, m_t;
+    T mas[N];
+    };
+
+
+    int main()
+    {
+        Array3d<int> mas(2, 2, 2);
+        mas.SetAll();// 1 2 3 4 5 6 7 8
+        mas.GetValues12(1, 0);
+        std::cout << std::endl;
+        std::cout << mas.Element(1, 1, 1);
+    }
+
---
+++ lab4/l4.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+
+struct ActionAndButton{
+	std::string act;
+	std::string button;
+};
+
+class Keyboard {
+   public:
+	   Keyboard() {
+		
+	   }
+	   ActionAndButton actionAndButton[100];
+	   int t = 0;
+
+
+
+	   void NewAct() {
+		   std::cout << "Введите название кнопки/кнопок" << std::endl;
+		   std::cin >> actionAndButton[t].button;
+		   std::cout << "Введите действие" << std::endl;
+		   std::cin >> actionAndButton[t].act;
+		   t++;
+	   }
+
+	   void PressButton(std::string s) {
+		   bool tmp = 0;
+		   for (int i = 0; i < t; i++) {
+			   if (s == actionAndButton[i].button) {
+				   std::cout << actionAndButton[i].act << std::endl;
+				   tmp = 1;
+				   i = t;
+			   }
+		   }
+		   if (tmp == 0) {
+			   std::cout << "У этой кнопки нет действия. Хотите его добавить" << std::endl;
+			   std::cout << "0. Нет" << std::endl;
+			   std::cout << "1. Да" << std::endl;
+			   int i;
+			   std::cin >> i;
+			   if (i == 1) {
+				   this->NewAct();
+			   }
+		   }
+	   }
+	   
+
+ 
+
+	   void Help() {
+		   for (int i = 0; i < t; i++) {
+			   std::cout << actionAndButton[i].button + " "+ actionAndButton[i].act << std::endl;
+		   }
+	   }
+
+	  
+private:
+	
+};
+
+class Workflow {
+public:
+	Workflow() {
+	}
+
+
+	void Undo(Keyboard& m_key) {
+		std::cout << "НА одно действие назад" << std::endl;
+		if (nnumberOfActions == 0) {
+			m_key.t--;
+		}
+		for (int i = 0; i < m_key.t; i++) {
+			if (Action[nnumberOfActions - 1].act == m_key.actionAndButton[i].act and Action[nnumberOfActions - 1].button == m_key.actionAndButton[i].button) {
+				nnumberOfActions--;
+				m_key.t--;
+				i = m_key.t;
+			}
+			else if (Action[nnumberOfActions - 1].act != m_key.actionAndButton[i].act and Action[nnumberOfActions - 1].button == m_key.actionAndButton[i].button) {
+				m_key.actionAndButton[i].act = Action[nnumberOfActions - 1].act;
+				nnumberOfActions--;
+				i = m_key.t;
+			}
+		}
+	}
+
+
+	void Reassignment(std::string s, Keyboard& m_key) {
+		std::cout << "Назначьте этой кнопке другое действие" << std::endl;
+		bool tmp = 0;
+		for (int i = 0; i < m_key.t; i++) {
+			if (s == m_key.actionAndButton[i].button) {
+				Action[nnumberOfActions] = m_key.actionAndButton[i];
+				nnumberOfActions++;
+				std::cin >> m_key.actionAndButton[i].act;
+				tmp = 1;
+				i = m_key.t;
+			}
+		}
+		if (tmp == 0) {
+			std::cout << "У этой кнопки нет действия. Хотите его добавить" << std::endl;
+			std::cout << "0. Нет" << std::endl;
+			std::cout << "1. Да" << std::endl;
+			int i;
+			std::cin >> i;
+			if (i == 1) {
+				m_key.NewAct();
+			}
+		}
+
+	}
+
+
+private:
+
+	ActionAndButton Action[200];
+	int nnumberOfActions = 0;
+};
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	Keyboard Key;
+	Workflow work;
+	Key.NewAct();
+	Key.NewAct();
+	Key.Help();
+	std::cout << "Введите клавишу, которую ранее нажали" << std::endl;
+	std::string s;
+	std::cin >> s;
+	work.Reassignment(s, Key);
+	Key.Help();
+	work.Undo(Key);
+	Key.Help();
+
+}
+
