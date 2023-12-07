f35a53c74b13e77c17739ae0a1de4b075d4afb16
Anton <assavich@stud.kantiana.ru>
2023-12-07 11:38:30 +0200

    .

---
+++ lab5/l5.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <vector>
+
+class User {
+public:
+    int id;
+    std::string name;
+    std::string login;
+    std::string password;
+};
+
+class IUserManger {
+public:
+    IUserManger();
+    void SignIn(User user) {
+        if (m_authorized == 0) {
+    
+                m_user = user;
+                m_authorized = 1;
+           
+        }
+        else if (user.login == m_user.login) {
+            std::cout << "ok";
+        }
+        else {
+            std::cout << "exit?(1.Yes  2.No)";
+            int tmpt;
+            std::cin >> tmpt;
+            if (tmpt == 1) {
+                this->SignOut();
+            }
+        }
+
+    }
+    void SignOut() {
+        m_authorized = 0;
+    }
+    bool IsAuthorized() {
+        return m_authorized;
+    }
+protected:
+    User m_user;
+    bool m_authorized = 0;
+};
+
+
+template<typename T>
+class IDataRepsoity {
+public:
+
+
+    std::vector<T> Get() {
+        return m_data;
+    }
+
+
+
+    void Add(T item) {
+        m_data.push_back(item);
+
+    }
+
+
+
+    void Remove(T item) {
+        for (int i = 0; i < m_data.size(); i++) {
+            if (item == m_data[i]) {
+
+                m_data.erase(i);
+            }
+        }
+    }
+
+    void Update(T item) {
+        m_data.clear();
+    }
+private:
+
+    std::vector<T>  m_data;
+
+};
+
+IDataRepsoity<User> dataBase;
+
+class IUserRepository  {
+
+
+    User GetById(int id) {
+
+        std::vector<User>  data = dataBase.Get();
+        for (int i = 0; i < data.size(); i++) {
+            if (data[i].id == id)
+                return data[i];
+        }
+
+    }
+    User GetByName(std::string name) {
+        std::vector<User>  data = dataBase.Get();
+        for (int i = 0; i < data.size(); i++) {
+            if (data[i].name == name)
+                return data[i];
+        }
+    }
+};
+
+class FileManager {
+public:
+    FileManager() {
+
+    }
+    void Create() {
+        std::string name;
+        std::string login;
+        std::string password;
+        std::cin >> name >> login >> password;
+        t++;
+        User user;
+        user.id = t;
+        user.login = login;
+        user.name = name;
+        user.password = password;
+        dataBase.Add(user);
+    }
+    void SignIn(User user) {
+        std::vector<User> data = dataBase.Get();
+        for (int i = 0; i < data.size(); i++) {
+            if (user.login == data[i].login && user.name == data[i].name && user.password == data[i].password) {
+                ium.SignIn(user);
+                break;
+            }
+        }
+    }
+
+    void SignOut() {
+        ium.SignOut();
+    }
+    
+
+    User GetById(int id) {
+        
+
+    }
+    User GetByName(std::string name) {
+
+    }
+    IUserRepository iur;
+
+private:
+    
+    IUserManger ium;
+    int t = 0;
+};
+
+int main()
+{
+ 
+
+}
---
+++ lab6/l6.cpp
@@ -0,0 +1,171 @@
+﻿#include <iostream>
+
+struct  Point
+{
+    int x;
+    int y;
+};
+
+class Control {
+public:
+    Control() {
+        std::cout << "Control" << std::endl;
+    }
+    virtual ~Control() {
+
+    }
+        virtual void setPosition(Point m)=0 {
+          
+        }
+        virtual Point getPosition() = 0 {
+           
+        }
+protected:
+    Point m_m;
+};
+
+
+
+class Form: public Control {
+public:
+    Form() :Control() {
+        std::cout << "Form" << std::endl;
+    }
+    ~Form() {
+
+    }
+   void setPosition(Point m) override {
+       m_m = m;
+    }
+  Point getPosition() override {
+      return m_m;
+    }
+    void addControl() {
+        std::cout << "addContoll" << std::endl;
+    }
+
+}; 
+
+
+class Label : public Control {
+public:
+    Label() :Control() {
+        std::cout << "Label" << std::endl;
+    }
+
+    ~Label() {
+
+    }
+    void setPosition(Point m) override {
+        m_m = m;
+    }
+    Point getPosition() override {
+        return m_m;
+    }
+    void setText(std::string t) {
+        std::cout << "setText" << std::endl;
+        m_t = t;
+    }
+    std::string getText() {
+        return m_t;
+    }
+protected:
+    std::string m_t;
+};
+
+
+class TextBox :public Control {
+public:
+    TextBox():Control() {
+        std::cout << "Textbox" << std::endl;
+   }
+    ~TextBox()  {
+
+    }
+
+    void setPosition(Point m) override {
+        m_m = m;
+    }
+    Point getPosition() override {
+        return m_m;
+    }
+    void setText(std::string t) {
+        m_t = t;
+   }
+    std::string getText() {
+        return m_t;
+    }
+    void OnValueChanged() {
+        std::cout << "OnValueChanged" << std::endl;
+    }
+protected:
+    std::string m_t;
+};
+
+
+class Factory {
+public:
+    Factory() {
+
+    }
+    ~Factory() {
+
+    }
+    virtual Label* createLabel() = 0;
+    virtual TextBox* createTextbox() = 0;
+    virtual Form* createForm() = 0;
+};
+
+
+
+
+class WindowsConrolFactory : public Factory {
+public:
+    WindowsConrolFactory() :Factory() {
+        std::cout << "WindowsControlFactory" << std::endl;
+    }
+    ~WindowsConrolFactory() {
+
+    }
+    Label *createLabel() override {
+        return new Label;
+    }
+    TextBox * createTextbox() override {
+        return new TextBox;
+    }
+    Form *createForm() override {
+        return new Form;
+    }
+};
+
+
+class LinuxConrolFactory : public Factory {
+public:
+    LinuxConrolFactory() :Factory() {
+        std::cout << "WindowsControlFactory" << std::endl;
+    }
+    ~LinuxConrolFactory() {
+
+    }
+    Label* createLabel() override {
+        return new Label;
+    }
+    TextBox* createTextbox() override {
+        return new TextBox;
+    }
+    Form* createForm() override {
+        return new Form;
+    }
+};
+
+int main()
+{
+
+    Factory* factory = new WindowsConrolFactory;
+    TextBox* t = factory->createTextbox();
+    t->OnValueChanged();
+    Factory* lFactory = new LinuxConrolFactory();
+    Form* f = lFactory->createForm();
+    f->addControl();
+    
+}
